ba666588c7aa8799462c54318cf265f5ca72121f
JKSTAFF <ionsuite@gmail.com>
2024-04-02 10:06:32 +0800

    fork 3rd-packages for different version

--- luci-app-arpbind/files/usr/lib/lua/luci/controller/arpbind.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.arpbind",package.seeall)
-function index()
-if not nixio.fs.access("/etc/config/arpbind")then
-return
-end
-entry({"admin","network","arpbind"},cbi("arpbind"),_("IP/MAC Binding"),45).dependent=true
-end
--- vlmcs/luci-app-vlmcsd/README.md
+++
@@ -1,2 +0,0 @@
-# luci-app-vlmcsd
-LuCI app for [openwrt-vlmcsd](https://github.com/mchome/openwrt-vlmcsd "")
